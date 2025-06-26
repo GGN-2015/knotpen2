685d5916ce6feb43c1a66010f9212939f082a8dd
GGN_2015 <premierbob@qq.com>
2025-06-26 11:13:05 +0800

    v2025-06-26

--- main.bat
+++ main.bat
@@ -2,4 +2,4 @@
 cd /d %~dp0
 echo %~dp0
 
-start emb_python\python.exe -c "import sys; sys.path=['.'] + sys.path; print(sys.path); import knotpen2; knotpen2.test_main.test_main()"
+emb_python\python.exe -c "import sys; sys.path=['.'] + sys.path; print(sys.path); import knotpen2; knotpen2.test_main.test_main()"
