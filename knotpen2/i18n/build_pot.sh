5df96bfe145ecbe2868ab0ccef46dc4fee87958c
GGN_2015 <premierbob@qq.com>
2025-07-16 14:24:08 +0800

    v2025-07-16

--- knotpen2/i18n/build_pot.sh
+++ knotpen2/i18n/build_pot.sh
@@ -8,3 +8,6 @@ SCRIPT_DIR="$(dirname "$SCRIPT_PATH")"
 cd "$SCRIPT_DIR" || exit
 
 xgettext -d knotpen2 -o locales/knotpen2.pot ../*.py --from-code=UTF-8
+mkdir -p locales/{zh_CN,en_US}/LC_MESSAGES
+msginit -i locales/knotpen2.pot -o locales/zh_CN/LC_MESSAGES/knotpen2.po -l zh_CN
+msginit -i locales/knotpen2.pot -o locales/en_US/LC_MESSAGES/knotpen2.po -l en_US
